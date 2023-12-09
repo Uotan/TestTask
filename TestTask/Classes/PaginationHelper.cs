153227c16bcee318e7ae109bba51c7546a0ab20e
Uotan Heinz <rezer.new@mail.ru>
2023-12-10 00:54:39 +0700

    Доделано второе задание с учетом замечаний

--- TestTask/Classes/PaginationHelper.cs
+++ TestTask/Classes/PaginationHelper.cs
@@ -6,39 +6,94 @@ using System.Threading.Tasks;
 
 namespace TestTask.Classes
 {
-    public class PaginationHelper
+    public class PaginationHelper<T>
     {
-        private readonly List<string> _data;
-        private readonly int _pageSize;
+        private readonly ICollection<T> _data;
+        public readonly int _pageSize;
         private readonly int _countOfPages;
-        public PaginationHelper(List<string> data, int pageSize)
+
+        /// <summary>
+        /// Утилита для пагинации
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="pageSize"></param>
+        public PaginationHelper(ICollection<T> data, int pageSize)
         {
             _data = data;
             _pageSize = pageSize;
-            _countOfPages = (int)_data.Count / _pageSize;
+            if ((_data.Count % pageSize)>0)
+            {
+                _countOfPages = (_data.Count / pageSize)+1;
+            }
+            else
+            {
+                _countOfPages = (_data.Count / pageSize);
+            }
         }
+
+        /// <summary>
+        /// Получает количество данных в массиве
+        /// </summary>
+        /// <returns></returns>
         public int GetCountOfData()
         {
             return _data.Count;
         }
+
+        /// <summary>
+        /// Получает количество страниц
+        /// </summary>
+        /// <returns></returns>
         public int GetPageCount()
         {
             return _countOfPages;
         }
+
+
+        /// <summary>
+        /// Получает колическо элементов на указанной странице
+        /// </summary>
+        /// <param name="pagenumber">Номер страницы (начинается с нуля)</param>
+        /// <returns></returns>
         public int GetCountOfElementsOnPage(int pageNumber)
         {
-            if (pageNumber<=(_countOfPages-1))
-            {
-                return _pageSize;
-            }
-            else
+            // было бы гораздо короче, если использовать _pageSize, но на половину сделаем вид, что не знаем количество элементов на странице
+            pageNumber++;
+            if (pageNumber>_countOfPages)
             {
                 return 0;
             }
+            if (pageNumber==1)
+            {
+                return pageNumber * _pageSize;
+            }
+            else if(pageNumber==_countOfPages)
+            {
+                return _data.Count() - ((pageNumber - 1) * _pageSize);
+            }
+            else
+            {
+                return pageNumber * _pageSize - (pageNumber - 1) * _pageSize;
+            }
         }
+
+        /// <summary>
+        /// Получает номер страницы, по индексу элемента массива данных
+        /// </summary>
+        /// <param name="itemIndex">Индекс элмента данных</param>
+        /// <returns></returns>
         public int GetIndexOfPage(int itemIndex)
         {
-            return itemIndex / _pageSize;
+            if (itemIndex>_data.Count)
+            {
+                return 0;
+            }
+            int pageIndex = itemIndex / _pageSize;
+            if ((itemIndex % _pageSize)!=0)
+            {
+                pageIndex++;
+            }
+            return pageIndex;
         }
     }
 }
--- Unit Tests/UnitTest2.cs
+++ Unit Tests/UnitTest2.cs
@@ -1,15 +1,64 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 using Tasks;
+using TestTask.Classes;
 
 namespace Unit_Tests
 {
     [TestClass]
     public class UnitTest2
     {
-        [TestMethod]
-        public void TestMethod1()
+        PaginationHelper<string> helper;
+        
+        void FillCollection()
         {
+            //создадим коллекцию строк, где каждая строка будет имет вид number_1, number_2 и т.д. до 17
+            ICollection<string> collection = new List<string>();
+            for (int i = 1; i <= 17; i++)
+            { 
+                collection.Add("number_"+i.ToString());
+            }
+            //количество элементов на странице будет 5, должно быть 4 страницы с данными
+            helper = new PaginationHelper<string>(collection, 5);
+        }
+
+        [TestMethod]
+        public void TestGetCountOfData()
+        {
+            FillCollection();
+            int result = helper.GetCountOfData();
+            Assert.AreEqual(17, result);
+        }
+
+        [TestMethod]
+        public void TestGetPageCount()
+        {
+            FillCollection();
+            int result = helper.GetPageCount();
+            Console.WriteLine(result);
+            Assert.AreEqual(4, result);
+        }
+
+
+        [TestMethod]
+        public void TestGetCountOfElementsOnPage()
+        {
+            FillCollection();
+            //с учетом нумерации от нуля отправим индекс последней страницы
+            int result = helper.GetCountOfElementsOnPage(3);
+            Console.WriteLine(result);
+            Assert.AreEqual(2, result);
+        }
+
+
+        [TestMethod]
+        public void TestGetIndexOfPage()
+        {
+            FillCollection();
+            int result = helper.GetIndexOfPage(14);
+            Console.WriteLine(result);
+            Assert.AreEqual(3, result);
         }
     }
 }
